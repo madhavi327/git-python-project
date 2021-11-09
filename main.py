6fd7c0843ac211da428a5913b931a68ca3360cf1
madhavi327 <93930816+madhavi327@users.noreply.github.com>
2021-11-09 07:22:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+'''program to find whether the given number is palindrome or not'''
+n=int(input("enter the number"))
+temp=n
+while (n>0):
+	r=n%10
+	rev=rev*10+r
+	n=n/10
+if n==temp:
+	print("yes")
+else:
